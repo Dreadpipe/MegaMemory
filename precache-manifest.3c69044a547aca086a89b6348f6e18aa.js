e35a3e650eaf3d2d6fca8741dc03b7c0d3294022
Dreadpipe <benharris83@gmail.com>
2019-06-10 19:58:18 -0500

    Updates

---
+++ precache-manifest.3c69044a547aca086a89b6348f6e18aa.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af08195acd76f58313237f42680bb469",
+    "url": "/megamemory/index.html"
+  },
+  {
+    "revision": "ba795b45ef773b7864c0",
+    "url": "/megamemory/static/css/main.781a9d4f.chunk.css"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/megamemory/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "ba795b45ef773b7864c0",
+    "url": "/megamemory/static/js/main.a83d5291.chunk.js"
+  },
+  {
+    "revision": "2504c3554e3ae8552991",
+    "url": "/megamemory/static/js/runtime~main.bcfc3fcb.js"
+  },
+  {
+    "revision": "377ea7042e4ba99292bed86d50808f15",
+    "url": "/megamemory/static/media/Airman.377ea704.png"
+  },
+  {
+    "revision": "1f84e7737f80aa16d28512193e15542f",
+    "url": "/megamemory/static/media/Bubbleman.1f84e773.png"
+  },
+  {
+    "revision": "095d1def56cbec86fb9d74b3c2419df2",
+    "url": "/megamemory/static/media/Crashman.095d1def.png"
+  },
+  {
+    "revision": "5e7ce27e8c73bc3dc50768a983dc7c19",
+    "url": "/megamemory/static/media/Flashman.5e7ce27e.png"
+  },
+  {
+    "revision": "5607d19a7fe0acd95b6e80b31a0d6042",
+    "url": "/megamemory/static/media/Heatman.5607d19a.png"
+  },
+  {
+    "revision": "6dde7cd45a615ad03523b23d68d95007",
+    "url": "/megamemory/static/media/Light.6dde7cd4.png"
+  },
+  {
+    "revision": "a42524ccb6764471e020c5462aa36f78",
+    "url": "/megamemory/static/media/Megaman.a42524cc.png"
+  },
+  {
+    "revision": "db74ae9b8beb6a414d72ebbac19b0a54",
+    "url": "/megamemory/static/media/Metalman.db74ae9b.jpg"
+  },
+  {
+    "revision": "698344651286858c4fa8f98acb75cfa9",
+    "url": "/megamemory/static/media/Protoman.69834465.png"
+  },
+  {
+    "revision": "1f45bd4c1af44f41b94a2a85316154f0",
+    "url": "/megamemory/static/media/Quickman.1f45bd4c.jpg"
+  },
+  {
+    "revision": "1cd694e6e6486f54013e4746007717f0",
+    "url": "/megamemory/static/media/Roll.1cd694e6.png"
+  },
+  {
+    "revision": "b6905d1bbecc9418bfa03d5462139cda",
+    "url": "/megamemory/static/media/Wily.b6905d1b.png"
+  },
+  {
+    "revision": "b1688116dd837e6bbfe8cd7870be23b4",
+    "url": "/megamemory/static/media/Woodman.b1688116.jpg"
+  }
+]);
